38968c61c7de9ad10cac958af5778b133abe5aca
yaoxin666 <2821226403@qq.com>
2025-07-17 18:44:07 +0800

    首页完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>
-我是一级路由首页
+<el-card>
+    <div class="box">
+      <img :src="userStore.avatar" alt="" class="avatar">
+      <div class="bottom">
+        <h3 class="title">{{ getTime() }}好呀{{ userStore.username }}</h3>
+        <p class="subtitle">姚鑫甄选运营平台</p>
+      </div>
+    </div>
+  </el-card>
+  <div class="bottoms">
+    <svg-icon name="welcome" width="600px" height="300px"></svg-icon>
   </div>
 </template>
 
 <script setup lang="ts">
+import { getTime } from '@/utils/time';
 import useUSerStore from '@/store/modules/user';
 import { onMounted } from 'vue';
 //获取仓库
@@ -15,6 +25,34 @@ onMounted(()=>{
   })
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.box {
+  display: flex;
 
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+
+  .bottom {
+    margin-left: 20px;
+
+    .title {
+      font-size: 30px;
+      font-weight: 900;
+      margin-bottom: 30px;
+    }
+
+    .subtitle {
+      font-style: italic;
+      color: skyblue;
+    }
+  }
+}
+.bottoms{
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
+}
 </style>
